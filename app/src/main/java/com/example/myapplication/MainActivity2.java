dbaeb9ebb9ff2c615d7ca513b078e4aefdf79a31
dizzaharkhani <dizzaharkhani1106@gmail.com>
2023-04-14 18:47:31 +0530

    Toolabar
    TOolbar add

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,12 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity2 extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initview();
     }
+
+    private void initview() {
+        toolbar= (Toolbar) findViewById(toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setLogo(R.drawable.home);
+    }
+
+    private Object findViewById(Toolbar toolbar) {
+        return null;
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+
+    }
+
 }
